dad66db8af784fe7ce18b2235cfb92cb29bbaaa8
hanba478 <hanba478@student.liu.se>
2022-04-29 15:12:14 +0200

    docstring

--- src/se/liu/hanba478henan555/game_director/sound_manager/SoundType.java
+++ src/se/liu/hanba478henan555/game_director/sound_manager/SoundType.java
@@ -1,5 +1,13 @@
 package se.liu.hanba478henan555.game_director.sound_manager;
 
+/**
+ * All background sounds and soundeffects
+ * A list of enums for every sound.
+ *
+ * for TDDD78 at LIU 2022-04-29
+ *	hanba478@student.liu.se
+ * 	henan555@student.liu.se
+ */
 public enum SoundType
 {
     MUSIC, PLING, DOOR, DEATH, HIT, SWORD, HEART
